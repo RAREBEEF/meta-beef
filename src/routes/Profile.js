9618cd79dd3e7658b5a14c30de224a2a5d12f09c
RAREBEEF <drrobot409@gmail.com>
2023-01-25 15:49:15 +0900

    내 글 불러오기에도 무한 스크롤 적용

--- src/routes/Profile.js
+++ src/routes/Profile.js
@@ -1,4 +1,4 @@
-import React, { useCallback, useEffect, useState } from "react";
+import React, { useCallback, useEffect, useRef, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { authService, dbService, storageService } from "../fbase";
 import { v4 as uuidv4 } from "uuid";
@@ -7,35 +7,81 @@ import styles from "./Profile.module.scss";
 import SubmitLoadingIon from "../icons/SubmitLoadingIcon.js";
 import defaultProfileImg from "../images/defaultProfileImg.png";
 import classNames from "classnames";
+import LoadingIcon from "../icons/LoadingIcon";
 
 export default function Profile({ userObj, refreshUser }) {
   const testerId = "ZsTy9n6HxzP0U0NUt2I3Cpf5FK82";
   const [newDisplayName, setNewDisplayName] = useState(userObj.displayName);
   const [attachment, setAttachment] = useState("");
-  const [myMebs, setMyMebs] = useState([]);
-  const [doUpdate, setDoUpdate] = useState(0);
   const [uploading, setUploading] = useState(false);
   const [alert, setAlert] = useState("");
-
   const navigate = useNavigate();
+  const [init, setInit] = useState(false);
+  const [loading, setLoading] = useState(false);
+  const loadMoreBtnRef = useRef(null);
+  const [mebs, setMebs] = useState([]);
+  const [limit, setLimit] = useState(10);
+  const [hasNextMeb, setHasNextMeb] = useState(true);
 
-  // 내 Meb들만 불러오기
-  const getMyMeb = useCallback(async () => {
-    const myMebData = await dbService
+  // meb 불러오기
+  const getMebs = useCallback(() => {
+    const unsub = dbService
       .collection("mebs")
       .where("creatorId", "==", userObj.uid)
       .orderBy("createdAt", "desc")
-      .get();
-    const myMebArray = myMebData.docs.map((doc) => ({
-      id: doc.id,
-      ...doc.data(),
-    }));
-    setMyMebs(myMebArray);
-  }, [userObj.uid]);
+      .limit(limit)
+      .onSnapshot((snapshot) => {
+        const data = snapshot.docs.map((doc) => ({
+          id: doc.id,
+          ...doc.data(),
+        }));
 
+        setMebs(data);
+        if (data.length + 30 < limit) setHasNextMeb(false);
+        setLoading(false);
+      });
+
+    return unsub;
+  }, [limit, userObj.uid]);
+
+  // 최초 로드
   useEffect(() => {
-    getMyMeb();
-  }, [doUpdate, getMyMeb]);
+    const unsub = getMebs();
+
+    return () => {
+      unsub();
+    };
+  }, [getMebs]);
+
+  // 최초 로드 완료시
+  useEffect(() => {
+    mebs.length !== 0 && setInit(true);
+  }, [mebs.length]);
+
+  // 더 불러오기
+  // 이미 불러오는 중이거나 다음 meb이 없을 경우 실행x
+  const onLoadMore = useCallback(() => {
+    if (loading || !hasNextMeb) return;
+    setLoading(true);
+
+    const increase = limit + 10;
+    getMebs(increase);
+    setLimit(increase);
+  }, [loading, getMebs, hasNextMeb, limit]);
+
+  // 인피니티 스크롤 옵저버 생성
+  useEffect(() => {
+    if (!init) return;
+
+    const scrollTrigger = new IntersectionObserver(
+      (entries) => {
+        entries[0].isIntersecting && onLoadMore();
+      },
+      { threshold: 1 }
+    );
+
+    loadMoreBtnRef.current && scrollTrigger.observe(loadMoreBtnRef.current);
+  }, [init, onLoadMore]);
 
   const onLogOutClick = () => {
     authService.signOut();
@@ -82,7 +128,7 @@ export default function Profile({ userObj, refreshUser }) {
 
       // 닉네임에 변경사항이 있을 경우
       if (newDisplayName !== "") {
-        myMebs.forEach((meb) => {
+        mebs.forEach((meb) => {
           if (meb.displayName !== newDisplayName) {
             dbService
               .doc(`mebs/${meb.id}`)
@@ -111,7 +157,7 @@ export default function Profile({ userObj, refreshUser }) {
         newProfileImgUrl = await response.ref.getDownloadURL();
 
         // 내 모든 글의 프사 변경
-        myMebs.forEach((meb) => {
+        mebs.forEach((meb) => {
           if (meb.profileImg !== newProfileImgUrl) {
             dbService
               .doc(`mebs/${meb.id}`)
@@ -132,7 +178,6 @@ export default function Profile({ userObj, refreshUser }) {
 
       // 새로고침
       refreshUser();
-      setDoUpdate((prev) => prev + 1);
     }
   };
 
@@ -249,21 +294,35 @@ export default function Profile({ userObj, refreshUser }) {
         </div>
       </form>
       <h2 className={styles["list-title"]}>내가 쓴 글</h2>
-      {myMebs.length === 0 ? (
+      {mebs.length === 0 ? (
         <div className={styles["no-mebs"]}>
           <div>작성한 글이 없습니다.</div>
           <div>첫 소식을 공유해 보세요.</div>
         </div>
       ) : (
-        myMebs.map((meb) => (
+        mebs.map((meb) => (
           <Meb
             key={meb.id}
             mebObj={meb}
             userObj={userObj}
             isOwner={meb.creatorId === userObj.uid}
-            setDoUpdate={setDoUpdate}
           />
         ))
+      )}
+      {loading ? (
+        <div
+          style={{ width: "100%", display: "flex", justifyContent: "center" }}
+        >
+          <LoadingIcon />
+        </div>
+      ) : (
+        <button
+          onClick={onLoadMore}
+          ref={loadMoreBtnRef}
+          style={{ width: "100%", pointerEvents: "none", opacity: 0 }}
+        >
+          load More
+        </button>
       )}
     </main>
   );
